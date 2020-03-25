1d5a0c7f53292775478ec7ad358d9a2fe3cdd022
kmcwharter <kristinmcwharter@gmail.com>
2020-03-24 19:49:09 -0500

    Create permission.js

---
+++ Tutorials/UnityWebGL/physicsDemo/permission.js
@@ -0,0 +1,24 @@
+if (typeof DeviceMotionEvent.requestPermission === 'function') {
+  // iOS 13+
+  DeviceMotionEvent.requestPermission()
+.then(response => {
+  if (response == 'granted') {
+    window.addEventListener('devicemotion', (e) => {
+      // do something with e
+    })
+  }
+})
+.catch(console.error)
+
+DeviceOrientationEvent.requestPermission()
+.then(response => {
+  if (response == 'granted') {
+    window.addEventListener('deviceorientation', (e) => {
+      // do something with e
+    })
+  }
+})
+.catch(console.error)
+} else {
+  // non iOS 13+
+}
