c406a3ce4c09f8e46ed264bb4c8349f2f5ad6c5f
kmcwharter <kristinmcwharter@gmail.com>
2020-03-24 20:02:26 -0500

    Update permission.js

--- Tutorials/UnityWebGL/physicsDemo/permission.js
+++ Tutorials/UnityWebGL/physicsDemo/permission.js
@@ -1,26 +1,14 @@
-function onClick() {
-    // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', () => {});
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
-    
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-      DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('devicemotion', () => {});
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
-  }
+function requestDeviceOrientation () {  
+  if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {  
+  DeviceOrientationEvent.requestPermission()  
+  .then(permissionState => {  
+  if (permissionState === 'granted') {  
+  window.addEventListener('deviceorientation', () => {});  
+  }  
+  })  
+  .catch(console.error);  
+  } else {  
+  // handle regular non iOS 13+ devices  
+  console.log ("not iOS");  
+  }  
+}  
